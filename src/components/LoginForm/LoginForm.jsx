0b317b6cc20e8a0e5b13a10dee0978ab2cc3a6e8
Jennifer Alexander <jenny_alexander@icloud.com>
2021-12-14 23:47:20 -0600

    Initial setup of component shells

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -73,43 +73,6 @@ function LoginForm() {
       </Container>
     </div>
   );
-  // return (
-  //   <form className="formPanel" onSubmit={login}>
-  //     <h2>Login</h2>
-  //     {errors.loginMessage && (
-  //       <h3 className="alert" role="alert">
-  //         {errors.loginMessage}
-  //       </h3>
-  //     )}
-  //     <div>
-  //       <label htmlFor="username">
-  //         Username:
-  //         <input
-  //           type="text"
-  //           name="username"
-  //           required
-  //           value={username}
-  //           onChange={(event) => setUsername(event.target.value)}
-  //         />
-  //       </label>
-  //     </div>
-  //     <div>
-  //       <label htmlFor="password">
-  //         Password:
-  //         <input
-  //           type="password"
-  //           name="password"
-  //           required
-  //           value={password}
-  //           onChange={(event) => setPassword(event.target.value)}
-  //         />
-  //       </label>
-  //     </div>
-  //     <div>
-  //       <input className="btn" type="submit" name="submit" value="Log In" />
-  //     </div>
-  //   </form>
-  // );
 }
 
 export default LoginForm;
