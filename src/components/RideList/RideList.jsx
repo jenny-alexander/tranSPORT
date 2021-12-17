3ce40302c55f774e4b8c7e70ea7015201ca706ce
Jennifer Alexander <jenny_alexander@icloud.com>
2021-12-16 23:46:06 -0600

    Add date and time to ride detail card

--- src/components/RideList/RideList.jsx
+++ src/components/RideList/RideList.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { Container, Grid, Card, CardContent, Rating, Typography, Button, Box } from '@mui/material';
-import CardHeader from '@mui/material/CardHeader';
-import CardAction from '@mui/material/CardAction';
-import Divider from '@mui/material/Divider'
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
 
 function RideList(props) {
   const [heading, setHeading] = useState('Functional Component');
@@ -32,7 +35,7 @@ function RideList(props) {
         props.filterByUser ? <h2>My Rides</h2> : <h2>All Rides</h2>
       }
       </div>
-      <h3>{JSON.stringify(rides)}</h3>
+      {/* <h3>{JSON.stringify(rides)}</h3> */}
       
       <Box>
         <Container>        
@@ -47,34 +50,33 @@ function RideList(props) {
                 return (
                         <Grid item xs={12}>
                           <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
-                            <Card elevation={3} sx={{width: '75%', p: 1, my: 0.5 }}>
-                              <Grid container direction="row" sx={{border:1}}>
-                                {/* <Grid item sx={6} sx={{border:1}}> */}
-                                  <CardHeader sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', border:1 }} 
-                                          title={new Date(ride.pickup_timestamp).toLocaleDateString()}
-                                          subheader={new Date(ride.pickup_timestamp).toLocaleTimeString( `en-US`, options )}
-                                          titleTypographyProps={{
-                                            variant: "h4",
-                                            align: "left"
-                                            }}
-                                            subheaderTypographyProps={{
-                                              variant: "h6",
-                                              align: "left",
-                                              color: "secondary",
-                                              gutterBottom: true
-                                            }}>
-                                  </CardHeader>
-                                {/* </Grid> */}
-                                  {/* <Grid item sx={2} sx={{border:1}}> */}
-                                    <CardContent sx={{border:1}}>
-                                        This is something
-                                    </CardContent>
-                                    <CardAction>
-                                      This is the action
-                                    </CardAction>
-                                {/* </Grid> */}
+                            <Paper elevation={3} sx={{width: '75%', p: 1, my: 0.5, borderRadius: 5}}>
+                              <Grid
+                                container
+                                direction="row"
+                                justifyContent="center"
+                                alignItems="center"
+                                spacing={3}> 
+                                <Grid item xs={6} sx={{padding:1}}>
+                                  <Typography sx={{fontSize: '20px'}} >                                    
+                                    {new Date(ride.pickup_timestamp).toLocaleDateString()}
+                                  </Typography>
+                                  <Typography sx={{fontSize: '14px'}} >
+                                  {new Date(ride.pickup_timestamp).toLocaleTimeString( `en-US`, options )}
+                                  </Typography>                                
+                                </Grid>
+                                <Grid item xs={6} sx={{border:1}}">
+                                  <List>
+                                    <ListItem>
+                                      <ListItemText
+                                        primary="Dropoff Location"
+                                        secondary="Driver"
+                                        />
+                                    </ListItem>
+                                  </List>                     
+                                </Grid>
                               </Grid>
-                            </Card>
+                            </Paper>
                           </Box>  
                         </Grid>                    
                 )
