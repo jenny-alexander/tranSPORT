a345bb38a5228a7ff13d07d0a186b4645aadb439
Jennifer Alexander <jenny_alexander@icloud.com>
2022-01-05 15:25:09 -0600

    Add About page details

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import { Box, ThemeProvider, createTheme } from '@mui/system';
-
-const theme = createTheme({
-  palette: {
-    background: {
-      paper: '#1D50D9',
-    },
-    text: {
-      primary: '##FDFEFE',
-      secondary: '#46505A',
-    },
-    action: {
-      active: '#001E3C',
-    },
-    success: {
-      dark: '#009688',
-    },
-  },
-});
+import './AboutPage.css';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -27,10 +17,41 @@ const theme = createTheme({
 function AboutPage() {
   return (
     <div>
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+      {/* <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
         <img src='images/logo2.png'></img>
-      </Box>
-      <h3>This is the about page</h3>
+      </Box> */}
+      <h2 className="title">About tranSPORT</h2>
+      <h3 className="tech-header">Technology Used:</h3>
+      <ul>
+        <li>HTML/JS</li>
+        <li>React</li>
+        <li>Redux-Saga</li>
+        <li>Express.js and Node.js</li>
+        <li>Material-UI</li>
+        <li>PostgreSQL</li>
+        <li>emailJS</li>
+        <li>Google MAPS Places API</li>
+      </ul>
+      <h3 className="next-header">Next Steps:</h3>
+      <div className="next-details">
+        <ul>
+          <li>Implement email functionality whereby users will receive an email if:
+            <ul>
+              <li>someone signs up to be a driver for their ride.</li>
+              <li>comments are added to a ride they are involved in.</li>
+              <li>a driver withdraws from a ride.</li>
+            </ul>
+          </li>
+          <li>Implement Google Maps Places Autocomplete functionality when a user enters a location
+            when creating a new ride request.
+          </li>
+        </ul>
+      </div>
+      <h3 className="thank-you-header">Thank You:</h3>
+      <ul>
+        <li>Ionian cohort members and Dev</li>
+        <li>Husband and kids</li>
+      </ul>
     </div>
 
 
